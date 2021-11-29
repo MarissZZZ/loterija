09709491aa5e8c45558f0cbab93d1abae096cbed
MarissZZZ <marisszuss@gmail.com>
2021-11-29 12:18:50 +0200

    Loterija

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const vardi= ['Līga Krūmiņa', 'Lauris Reiniks', 'Pīters Pārkers', 'Anna Failora', 'James Bonds'];
-const balvas= ['Thanos laika roka', 'Lavas lampa', 'Elektriskā tējkanna', 'Tesla', 'Zābaki'];
-const naudaKopa= 1000000;
+const vardi = ['Līga Kokle', 'Lauris Reiniks', 'Pīters Pārkers', 'Anna Failora', 'James Bonds'];
+const balvas = ['Thanos laika roka', 'Lavas lampa', 'Elektriskā tējkanna', 'Tesla', 'Zābaki'];
+const naudaKopa = 1000000;
 let uzvaretajuSkaits = 3;
+let rindas = document.querySelector('.rindas'); // pievieno mainīgo
 
-Math.random=Math.random()*10;
+// Math.random=Math.random()*10;
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+
+    let rand = Math.random() * vardi.length; // vārdu skaits mainīgs
+    rand = Math.floor(rand); // noapaļo uz leju
+
+    console.log(vardi[rand]);
+    //index.html meta cods nozīmē kad atļautas ir garumzīme
+
+    let uzvaretajs = vardi[rand]; // console.log vietā
+    rindas.innerHTML += `
+<tr>
+    <td>${i + 1}</td>
+    <td>${uzvaretajs}</td>
+</tr>` // pievieno +  
+
+}
+
+
