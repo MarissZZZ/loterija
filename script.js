bd91a508077bae9c033cb49748d435a661617f59
MarissZZZ <marisszuss@gmail.com>
2021-11-29 11:01:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert(1)
+const vardi= ['Līga Krūmiņa', 'Lauris Reiniks', 'Pīters Pārkers', 'Anna Failora', 'James Bonds'];
+const balvas= ['Thanos laika roka', 'Lavas lampa', 'Elektriskā tējkanna', 'Tesla', 'Zābaki'];
+const naudaKopa= 1000000;
+let uzvaretajuSkaits = 3;
+
+Math.random=Math.random()*10;
